092fc3f950935ce63fd65a6d9a7eab2f0c643f37
criscentij <48447031+criscentij@users.noreply.github.com>
2019-03-25 11:27:25 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410_final_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Submit(String firstname, String lastname)
+        {
+            NameValueCollection nvc = Request.Form;
+            String name = nvc["class"];
+            return Content("Test");
+        }
+
+    }
+}
